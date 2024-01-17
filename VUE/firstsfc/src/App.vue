a66547ece613ca4028b492b5c268b31079db4e58
Luke Second Comp <luk.chukotkin@bk.ru>
2024-01-17 13:32:49 +0300

    VUE/firstsfc/src: Vue components

--- VUE/firstsfc/src/App.vue
+++ VUE/firstsfc/src/App.vue
@@ -1,19 +1,26 @@
 
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1>Food</h1>
+  <food-item />
+  <food-item />
+  <food-item />
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        message: 'This is someone text!!!'
-      }
-    },
-  }
+  
 </script>
 
 <style>
+  #app > div {
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+    border: 1px dashed #000;
+  }
 
+  #app > div:hover {
+    cursor: pointer;
+  }
 </style>
